7c0f0540cc0804c1c4d86ab3f585df17adaca17e
Montagu-Evans <montagu-evans@hotmail.com>
2016-01-25 16:26:44 +0100

    Jag har laddat hem och restorat northwind, skapat ett projekt och kopierat in mdf-filen. Jag har även lagt till en ny wcf service

---
+++ WcfServiceDemo/SelfServiceHosting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceDemo;
+
+namespace SelfServiceHosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Service1)))
+            {
+                host.Open();
+                Console.WriteLine($"Service started {DateTime.Now.ToShortTimeString()}");
+                Console.WriteLine("Press ENTER to terminate!");
+                Console.ReadLine();   
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.ServiceReference1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private ServiceReference1.Service1Client client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new Service1Client();
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            if (client.State == CommunicationState.Faulted)
+            {
+                client = new Service1Client();
+            }
+
+            client = new Service1Client("NetTcpBinding_IService1");
+            client.GetData(4);
+        }
+    }
+}
