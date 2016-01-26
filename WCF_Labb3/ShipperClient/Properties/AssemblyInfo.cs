bca7430e1f44fdc832b07ae332241e05e3b42b66
Montagu-Evans <montagu-evans@hotmail.com>
2016-01-26 11:30:37 +0100

    Genererat proxy och config-fil

--- WCF_Labb3/ShipperClient/Properties/AssemblyInfo.cs
+++ WCF_Labb3/ShipperClient/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c7cf55d-391e-4426-9b9f-9abeb796317e")]
+[assembly: Guid("bfc6fcb3-38a9-4ea5-88ba-c2c0967f41aa")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
